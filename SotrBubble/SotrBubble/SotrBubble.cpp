31dc0f8f0345909f37e1196f715ab711f025c5aa
Adrian <adrian.chrzaszcz02@gmail.com>
2024-08-25 22:44:05 +0200

    AddBubbleSort

---
+++ SotrBubble/SotrBubble/SotrBubble.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void bubbleSort(vector <int>& AAA)
+{
+int length = AAA.size();
+
+	for (int i = 0; i < length-1; i++)
+	{
+		for (int j = 0; j < length-1-i; j++)
+		{
+			if (AAA[j] > AAA[j + 1])
+			{
+				swap(AAA[j], AAA[j + 1]);
+			}
+		}
+	}
+
+
+//	for (int z = 0; z < length; z++)
+//	{
+//		cout << AAA[z] << " ";
+//	}
+};
+
+void print(vector <int>& AAA)
+{
+	for (const auto& x : AAA)
+	{
+		cout << x << " ";
+	}
+}
+
+
+int main()
+{
+	vector<int> ZZZ{ 9, 3, 4, 8, 10, 7, 10, 30, 4, 1 };
+	bubbleSort(ZZZ);
+	print(ZZZ);
+}
