bec666f3fa006b942c694e814efde0de1e1935ba
Adrian <adrian.chrzaszcz02@gmail.com>
2024-09-06 13:42:51 +0200

    AddFibonaciiSequence

---
+++ Fibonacci sequence/Fibonacci sequence/Fibonacci sequence.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void AllFibonacciNumbers(int n)
+{
+    unsigned long a = 0, b = 1;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << b << " ";
+        b += a;
+        a = b - a;
+    }
+}
+
+
+int main()
+{
+    int n;
+
+    cout << "How many numbers of Fibonacci sequence do you want to see: ";
+    cin >> n;
+
+    AllFibonacciNumbers(n);
+
+    return 0;
+}
