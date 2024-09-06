e011e3ae66a322c66e56a15c02bde5b0d7911006
Adrian <adrian.chrzaszcz02@gmail.com>
2024-09-06 13:23:08 +0200

    AddNWD

---
+++ NWD/NWD/NWD.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int NWD(int First, int Second)
+{
+
+    if (First < Second)
+        swap(First, Second);
+
+    int Mod = First % Second;
+
+    if (Mod > 0)
+    {
+        First = Mod;
+
+        NWD(First, Second);
+    }
+
+    return Mod;
+}
+
+int main()
+{
+    int x, y;
+
+    cout << "Insert two numbers: " << endl;
+
+    cin >> x >> y;
+
+    cout << "NWD(" << x <<"," << y << ") = " << NWD(x, y);
+}
